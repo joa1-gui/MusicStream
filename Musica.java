3c2575054a928323e6f0670db7fb50f67a4dc8d0
João Guilherme <joaosantanaguilherme@gmail.com>
2026-05-19 21:42:36 -0300

    Update Musica.java

--- Musica.java
+++ Musica.java
@@ -1,5 +1,11 @@
 public class Musica{
 
-    
+   String titulo;
+    String artista;
+
+    public Musica(String titulo, String artista){
+        this.titulo = titulo;
+        this.artista = artista;
+    } 
     
 }
